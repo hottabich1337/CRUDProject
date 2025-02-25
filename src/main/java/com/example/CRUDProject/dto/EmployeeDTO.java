11236a43382c5e4aba0116aaa86b1662558be22c
hottabich1337 <hottabich1337@mail.ru>
2025-02-25 18:24:36 +0300

    EmployeeDTO  реализовать с MapStruct-om

---
+++ src/main/java/com/example/CRUDProject/dto/EmployeeDTO.java
@@ -0,0 +1,63 @@
+package com.example.CRUDProject.dto;
+
+import com.example.CRUDProject.entity.Employee;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class EmployeeDTO {
+    private String name;
+    private String surname;
+    private String email;
+    private String password;
+    private String role;
+    public EmployeeDTO(Employee employee) {
+        this.name = employee.getName();
+        this.surname = employee.getSurname();
+        this.email = employee.getEmail();
+        this.password = employee.getPassword();
+        this.role = employee.getRole();
+    }
+
+    public EmployeeDTO() {}
+    public String getName() {
+        return name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
--- src/main/java/com/example/CRUDProject/entity/Employee.java
+++ src/main/java/com/example/CRUDProject/entity/Employee.java
@@ -1,5 +1,6 @@
 package com.example.CRUDProject.entity;
 
+import com.example.CRUDProject.dto.EmployeeDTO;
 import jakarta.persistence.*;
 
 
@@ -27,6 +28,10 @@ public class Employee {
     @Column(name = "role", nullable = false, length = 255)
     private String role;
 
+    public EmployeeDTO convertToDTO(Employee employee) {
+        return new EmployeeDTO(employee);
+    }
+
 
     public Integer getId() {
         return id;
@@ -50,5 +55,25 @@ public class Employee {
 
     public String getRole() {
         return role;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
     }
 }
